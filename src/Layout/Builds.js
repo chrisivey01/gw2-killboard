4ff029379249cdbc194172f358f44f9131201013
chris <chrisivey01@gmail.com>
2018-12-15 18:34:25 -0600

    Build updates

--- src/Layout/Builds.js
+++ src/Layout/Builds.js
@@ -1,4 +1,4 @@
-import React,{Component} from 'react'
+import React, {Component} from 'react'
 import buildLogo from '../Components/Builds/Wvw_fp.jpg'
 import guardianLogo from '../Components/Builds/Guardian_icon.png'
 import elementalistLogo from '../Components/Builds/Elementalist_icon.png'
@@ -10,61 +10,71 @@ import mesmerLogo from '../Components/Builds/Mesmer_icon.png'
 
 import {Modal, Button} from 'react-bootstrap'
 
-class Builds extends Component{
+class Builds extends Component {
 
-    constructor(props, context){
-        super(props,context)
-        this.state ={
-            show:false
+    constructor(props, context) {
+        super(props, context)
+        this.state = {
+            show: false
         };
     }
 
     handleClose = () => {
-        this.setState({ show: false});
+        this.setState({show: false});
     }
 
     handleShow = () => {
-        this.setState({ show: true})
+        this.setState({show: true})
     }
 
     loadGuardian = () => {
-        window.location = "http://gw2skills.net/editor/?vVAQNAW3ensADtChdDBOCDkCjlEC7aZFAOA7B0DaDFrBkBKTA-jlxHQBg7iA6GAoyPA8EAeoSwfs/A8U/hUA+NzC-w";
+        window.location = "http://gw2skills.net/editor/?vVAQNAW3ensADtChdDBOCDkCjlEC7aZFAOA7B0DaDFrBkBKTA-j1BDQBiUJ4AV/BAXAgJ8EAm3BB0NIl9HmoyPSB43ML-w";
     }
 
-    loadMesmer= () => {
-        window.location = "http://gw2skills.net/editor/?vhAQRAsd8en0nB9phVpBGoBEgiFij6sCugJwMAClFq+S/0SF-jFx/gAA6HAV+9qEES9HSBYiKN-w";
-    }
+    // loadMesmer = () => {
+    //     window.location = "http://gw2skills.net/editor/?vhAQRAsd8en0nB9phVpBGoBEgiFij6sCugJwMAClFq+S/0SF-jFx/gAA6HAV+9qEES9HSBYiKN-w";
+    // }
 
     loadNecro = () => {
-        window.location = "http://gw2skills.net/editor/?vRAQNAndWnE9CN9it0A20As9iFcBDaAEAOA6H2I7klqL0GeWA-jFSBQBa4JAAgTAwCOEAmUJY+0Hwv9HyoyvEq+DZA0HDA-w";
+        window.location = "http://gw2skills.net/editor/?vRAQNAndWnE9CN9it0A20As9iFcBjqLEH+CQDgAwBQ/wGZnsA-jFSBQBA4EAkQ1fMpSw8pPQDPBgFcIASy+DZU5HZA0HDA-w";
     }
 
     loadRevenant = () => {
-        window.location = "http://gw2skills.net/editor/?vlAQJASGW0UI7BewHqOjb8j2BEgHA-jFh/gAgK/gU/pVJIguAA-w";
+        window.location = "http://gw2skills.net/editor/?vlAQJASGW0UI7BewH2OgA8A1ZcjfA-jFSBQBJU9HAcBA0wTAIf6CMpSwceIA23+DZU5HA-w";
     }
 
-    loadElementalist = () => {
+    loadDDElementalist = () => {
+        window.location = "http://gw2skills.net/editor/?vFAQJAoYnMMANYiFYCeOAcYilNAjYFcBqAu71/6hd4BEAKgA-jlxHQBwT9HAq87LnAAA8AAAnegfs/AA-w";
+    }
+
+    loadStaffElementalist = () => {
         window.location = "http://gw2skills.net/editor/?vFAQFAWnMMAVOg94CeOAM5iFBALIAUAeA3r6Z9tW4SeBSBA-jFh/gAA6AAV+Bp+TrSQA-w";
     }
 
     loadWarrior = () => {
-        window.location = "http://gw2skills.net/editor/?vJAQNBiYD7kKRoMEsYXwKEgZXskFY4V4N8IWpGk6iBIDuCQAA-jFh/gAtKBBV+BobAS9HA-w";
+        window.location = "http://gw2skills.net/editor/?vlAQJASGW0UI1Zcjf0OgA8A7BewHC-jFSBQBJU9Has/Q+0FohnAgJVCCgLAQOPEAZU5HA-w";
     }
-    render(){
+
+    render() {
 
         let modal = {
-            height:'50%',
-            overflow:'auto'
+            height: '50%',
+            overflow: 'auto'
         }
 
         let flex = {
-            display:'flex'
+            display: 'flex'
+        }
+
+        let flexCenter = {
+            display: 'flex',
+            justifyContent:'space-around'
         }
 
         let text = {
-            fontSize:'9px'
+            fontSize: '12px'
         }
-        return(
+        return (
             <div className="sidebar">
                 <a id="builds" role="button" onClick={this.handleShow} data-toggle="modal"> <img src={buildLogo}/></a>
 
@@ -75,22 +85,61 @@ class Builds extends Component{
                                 <label>WvW Builds</label>
                             </div>
                             <div style={text}>
-                                <label>Click the icon of the class you want to see.</label>
+                                <div>
+                                    <label>Click the icon of the class you want to see.</label>
+                                </div>
+                                <div>
+                                    <label>Preferred classes currently as of 12/15/2018</label>
+                                </div>
+                                <div style={flexCenter}>
+                                    <div>
+                                        <ul>
+                                            <li>
+                                                Guardian/Firebrand
+                                            </li>
+                                            <li>
+                                                Revenant/Herald
+                                            </li>
+                                            <li>
+                                                Necro/Scourge
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div>
+                                        <ul>
+                                            <li>
+                                                Warrior/Spellbeaker
+                                            </li>
+                                            <li>
+                                                Elemenalist/Tempest
+                                            </li>
+                                            <li>
+                                                Elemenalist/Weaver
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
                         </Modal.Title>
                     </Modal.Header>
 
-                    <Modal.Body >
+                    <Modal.Body>
                         <div>
                             <div style={flex}>
-                                Firebrand: <a id="builds" role="button" onClick={this.loadGuardian} data-toggle="modal"> <img src={guardianLogo}/></a>
-                                Chrono: <a id="builds" role="button" onClick={this.loadMesmer} data-toggle="modal"> <img src={mesmerLogo}/></a>
-                                Scourge: <a id="builds" role="button" onClick={this.loadNecro} data-toggle="modal"> <img src={necromancerLogo}/></a>
+                                Firebrand: <a id="builds" role="button" onClick={this.loadGuardian} data-toggle="modal">
+                                <img src={guardianLogo}/></a>
+                                DD/DF Elementalist<a id="builds" role="button" onClick={this.loadDDElementalist} data-toggle="modal"> <img
+                                src={mesmerLogo}/></a>
+                                Scourge: <a id="builds" role="button" onClick={this.loadNecro} data-toggle="modal"> <img
+                                src={necromancerLogo}/></a>
                             </div>
                             <div style={flex}>
-                                Revenant: <a id="builds" role="button" onClick={this.loadRevenant} data-toggle="modal"> <img src={revenantLogo}/></a>
-                                Elementalist: <a id="builds" role="button" onClick={this.loadElementalist} data-toggle="modal"> <img src={elementalistLogo}/></a>
-                                Warrior: <a id="builds" role="button" onClick={this.loadWarrior} data-toggle="modal"> <img src={warriorLogo}/></a>
+                                Revenant: <a id="builds" role="button" onClick={this.loadRevenant} data-toggle="modal">
+                                <img src={revenantLogo}/></a>
+                                Staff Elementalist: <a id="builds" role="button" onClick={this.loadStaffElementalist}
+                                                 data-toggle="modal"> <img src={elementalistLogo}/></a>
+                                Warrior: <a id="builds" role="button" onClick={this.loadWarrior} data-toggle="modal">
+                                <img src={warriorLogo}/></a>
                             </div>
                         </div>
                     </Modal.Body>
@@ -101,7 +150,6 @@ class Builds extends Component{
             </div>
         )
     }
-
 
 
 }
